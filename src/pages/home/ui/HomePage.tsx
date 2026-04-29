053e1afcbeafe766dbbbd28bc3f2eeda9830e65a
umerovzafar <umerov.z99@gmail.com>
2026-04-30 00:34:15 +0500

    add tickets page

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,19 +1,8 @@
 import { HomeProvider } from '../model/HomeContext';
 import { HomePageView } from './HomePageView';
-import { HomePageHub } from './HomePageHub';
 
-
-export const HOME_HUB_ONLY = true;
-
-function HomePageWithTickets() {
+export function HomePage() {
     return (<HomeProvider>
       <HomePageView />
     </HomeProvider>);
-}
-
-export function HomePage() {
-    if (HOME_HUB_ONLY) {
-        return <HomePageHub />;
-    }
-    return <HomePageWithTickets />;
 }
